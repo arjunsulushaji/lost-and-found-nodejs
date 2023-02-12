0d9dc62afec576995f58912731f16f6857ea15c1
arjun <arjunshajiforpc@gmail.com>
2023-02-12 22:56:32 +0530

    found things layout setup in home page of user and dummy data passed during rendering

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,48 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  let items = [
+    {
+      lastDigit: 4444
+    },
+    {
+      lastDigit: 4455
+    },
+    {
+      lastDigit: 5555
+    },
+    {
+      lastDigit: 7788
+    },
+    {
+      lastDigit: 1122
+    },
+    {
+      lastDigit: 1245
+    }
+  ]
+  let itemss = [
+    {
+      lastDigit: 4444
+    },
+    {
+      lastDigit: 4455
+    },
+    {
+      lastDigit: 5555
+    },
+    {
+      lastDigit: 7788
+    },
+    {
+      lastDigit: 1122
+    },
+    {
+      lastDigit: 1245
+    }
+  ]
+  res.render('user/index', { items,itemss });
 });
 
 module.exports = router;
